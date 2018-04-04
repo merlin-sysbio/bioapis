813f83146e2775ba15866931d12033af91f66b0e
amaromorais <joseamaromorais@hotmail.com>
2018-04-04 16:31:34 +0100

    Import model source enumerator

--- src/main/java/pt/uminho/sysbio/common/bioapis/externalAPI/utilities/Enumerators.java
+++ src/main/java/pt/uminho/sysbio/common/bioapis/externalAPI/utilities/Enumerators.java
@@ -65,4 +65,21 @@ public class Enumerators {
 		}
 		
 	}
+	
+	public enum ModelSources{
+		
+		MODEL_SEED("ModelSEED smbl model");
+		
+		private String source;
+		
+		private ModelSources(String modelSource){
+			this.source = modelSource;
+		}
+		
+		public String source(){
+			return this.source;
+		}
+		
+	}
+	
 }
